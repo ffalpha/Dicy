eaca1c8f42b50b9f9db5719b8c604fb85e159856
ffalpha <kalanam217@gmail.com>
2022-02-02 18:31:33 +0530

    Add index file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import theme from "./theme";
+import { ThemeProvider, CssBaseline, Grid } from '@material-ui/core'
 const App = () => {
-  return <div></div>;
+  return <ThemeProvider theme={theme}> 
+  <CssBaseline />
+  
+  <div></div></ThemeProvider>;
 };
 
 export default App;
